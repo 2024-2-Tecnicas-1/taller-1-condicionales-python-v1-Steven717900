9d0d522ba05108da1e1214daee725c5a9fd1e9ef
Steven717900 <pospinachico@gmail.com>
2024-09-08 16:26:26 -0500

    solucion ordenamiento

--- src/ordenamiento.py
+++ src/ordenamiento.py
@@ -1,16 +1,19 @@
 def evaluar(numero1, numero2, numero3, numero4):
-    # TODO: Coloca aquí el código del ejercicio 5: Ordenamiento
-    return "";
 
-if __name__ == '__main__':
-    print("Número 1:", end="")
+    numeros = [numero1, numero2, numero3, numero4]
+    numeros.sort()
+    respuesta = ' '.join(map(str, numeros))
+    return respuesta
+
+if _name_ == '_main_':
+    print("Número 1:", end=" ")
     numero1 = int(input())
-    print("Número 2:", end="")
+    print("Número 2:", end=" ")
     numero2 = int(input())
-    print("Número 3:", end="")
+    print("Número 3:", end=" ")
     numero3 = int(input())
-    print("Número 4:", end="")
+    print("Número 4:", end=" ")
     numero4 = int(input())
-        
+    
     respuesta = evaluar(numero1, numero2, numero3, numero4)
-    print(respuesta)
+    print(f"Números ordenados: {respuesta}")
