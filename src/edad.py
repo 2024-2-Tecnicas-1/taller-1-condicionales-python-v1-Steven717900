a5e074c75dcb949e38d5403c4b99f04875bbe6e7
Steven717900 <pospinachico@gmail.com>
2024-09-08 16:25:05 -0500

    solucion edad

--- src/edad.py
+++ src/edad.py
@@ -1,15 +1,27 @@
-def evaluar(dia, mes, anno):
-    # TODO: Coloca aquí el código del ejercicio 6: Edad
-    return "";
+from datetime import datetime
 
-if __name__ == '__main__':
-    print("Ingrese su fecha de nacimiento")
-    print("Día:", end="")
-    dia = int(input())
-    print("Mes:", end="")
-    mes = int(input())
-    print("Año:", end="")
-    anno = int(input())
+def evaluar(dia, mes, anno):
+
+        fecha_actual = datetime.now()
+        fecha_nacimiento = datetime(anno, mes, dia)
         
+        edad = fecha_actual.year - fecha_nacimiento.year
+        
+        if (fecha_actual.month, fecha_actual.day) < (fecha_nacimiento.month, fecha_nacimiento.day):
+            edad -= 1
+        
+        respuesta = f"Usted tiene {edad} años"
+        return respuesta
+    
+
+if _name_ == '_main_':
+    print("Ingrese su fecha de nacimiento")
+    print("Día:", end=" ")
+    dia = int(input())
+    print("Mes:", end=" ")
+    mes = int(input())
+    print("Año:", end=" ")
+    anno = int(input())
+    
     respuesta = evaluar(dia, mes, anno)
     print(respuesta)
