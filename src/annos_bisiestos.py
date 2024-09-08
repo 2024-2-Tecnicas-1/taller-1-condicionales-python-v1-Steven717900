a41a27dcd2df4355ba29d0435318a0609f8cd5f8
Steven717900 <pospinachico@gmail.com>
2024-09-08 16:22:03 -0500

    Solucion anos bisiestos

--- src/annos_bisiestos.py
+++ src/annos_bisiestos.py
@@ -1,8 +1,19 @@
 def evaluar(anno):
     # TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-    return "";
+    if anno >= 1582:
+        if anno % 400 == 0 or (anno % 4 == 0 and anno % 100 != 0):
+            respuesta = f"{anno} es bisiesto"
+        else:
+            respuesta = f"{anno} no es bisiesto"
+    else:
+        if anno % 4 == 0:
+            respuesta = f"{anno} es bisiesto"
+        else:
+            respuesta = f"{anno} no es bisiesto"
 
-if __name__ == '__main__':
+    return respuesta
+            
+if _name_ == '_main_':
     print("Año:", end="")
     anno = int(input())
 
