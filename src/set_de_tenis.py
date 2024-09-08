1c9ed0d324e4f8909898b4fbb104d2fe343f9b6e
Steven717900 <pospinachico@gmail.com>
2024-09-08 16:25:37 -0500

    solucion set de tenis

--- src/set_de_tenis.py
+++ src/set_de_tenis.py
@@ -1,8 +1,24 @@
 def evaluar(num_victorias_a, num_victorias_b):
     # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
 
-if __name__ == '__main__':
+    if (num_victorias_a >= 7 and num_victorias_b <= 4) or (num_victorias_b >= 7 and num_victorias_a <= 4) or num_victorias_a > 7 or num_victorias_b > 7:
+        respuesta = "Inválido"
+    elif num_victorias_a >= 6 and (num_victorias_a - num_victorias_b) >= 2:
+        respuesta = "Ganó A"
+    elif num_victorias_b >= 6 and (num_victorias_b - num_victorias_a) >= 2:
+        respuesta = "Ganó B"
+    elif num_victorias_a == 7 and num_victorias_b == 6:
+        respuesta = "Ganó A"
+    elif num_victorias_b == 7 and num_victorias_a == 6:
+        respuesta = "Ganó B"
+    elif (num_victorias_a < 6 and num_victorias_b < 6) or (num_victorias_a == 5 and num_victorias_b == 5) or (num_victorias_a == 6 and num_victorias_b == 6) or (num_victorias_a == 6 and num_victorias_b == 5) or (num_victorias_a == 5 and num_victorias_b == 6):
+        respuesta = "Aún no termina"
+    else:
+        respuesta = "Inválido"
+    
+    return respuesta
+
+if _name_ == '_main_':
     print("Los juegos ganaddor por A:", end="")
     num_victorias_a = int(input())
     print("Los juegos ganaddor por B:", end="")
