6ffcfee987be0c344001530c6572ece6dbf3cb31
Steven717900 <pospinachico@gmail.com>
2024-09-08 16:23:46 -0500

    Solucion IMC

--- src/imc.py
+++ src/imc.py
@@ -1,8 +1,20 @@
 def evaluar(peso, estatura, edad):
-    # TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
-    return "";
+        # TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
+    IMC = peso / (estatura ** 2)
+    if edad < 45:
+        if IMC < 22.0:
+            respuesta = "bajo"
+        else:
+            respuesta = "medio"
+    else:
+        if IMC < 22.0:
+            respuesta = "medio"
+        else:
+            respuesta = "alto"
+    
+    return respuesta
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     print("Peso:", end="")
     peso = int(input())
     print("Estatura:", end="")
